bef5d78a6b84e18d452f1a161da7b77548a7bce9
Erimateia Lima <erimarlimasi@gmail.com>
2019-07-15 11:53:49 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,11 +2,25 @@
 
 require_once("config.php");
 
-$usuario = new Usuario();
+//Carrega um usuário
+//$usuario = new Usuario();
+//$usuario->loadById(1);
+//echo $usuario;
 
-$usuario->loadById(1);
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de usuários buscando pelo gonhin
+//$search = Usuario::search("user");
+//echo json_encode($search);
+
+//carrega o usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user","12345");
 
 echo $usuario;
+
 /*
 echo "<br>";
 echo "<br>";
