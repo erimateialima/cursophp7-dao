8a79d1391a52c85036c2e3b9875ad7e8e8f07db3
Erimateia Lima <erimarlimasi@gmail.com>
2019-07-15 16:47:56 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -16,10 +16,23 @@ require_once("config.php");
 //echo json_encode($search);
 
 //carrega o usuario usando o login e a senha
-$usuario = new Usuario();
-$usuario->login("user","12345");
+//$usuario = new Usuario();
+//$usuario->login("user","12345");
+//echo $usuario;
 
-echo $usuario;
+//insere um novo usuário
+//$aluno = new Usuario("professor", "123456");
+//$aluno = new Usuario();
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("12345");
+//$aluno->insert();
+//echo $aluno;
+
+//atualizar usuário
+//$usuario = new Usuario();
+//$usuario->loadById(3);
+//$usuario->update("professor","12345");
+//echo $usuario;
 
 /*
 echo "<br>";
